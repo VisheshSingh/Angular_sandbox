cf37cb6940ee05366c873abd9332af95fa631ae3
Vishesh <visheshsingh16@gmail.com>
2018-03-08 11:40:15 -0600

    ngIf structural directive

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -8,52 +8,55 @@ import { User } from '../model/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
   constructor() { }
 
   ngOnInit() {
     this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-          street: '123 main st',
-          city: 'Boston',
-          state: 'MA'
-        }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Smith',
-        age: 25 ,
-        address: {
-          street: '55 main st',
-          city: 'New York',
-          state: 'NY'
-        }
-      },
-      {
-        firstName: 'Peter',
-        lastName: 'Hung',
-        age: 38,
-        address: {
-          street: '555 New Road',
-          city: 'Princeton',
-          state: 'NJ'
-        }
-      }
+      // {
+      //   firstName: 'John',
+      //   lastName: 'Doe',
+      //   age: 30,
+      //   address: {
+      //     street: '123 main st',
+      //     city: 'Boston',
+      //     state: 'MA'
+      //   }
+      // },
+      // {
+      //   firstName: 'Kevin',
+      //   lastName: 'Smith',
+      //   age: 25 ,
+      //   address: {
+      //     street: '55 main st',
+      //     city: 'New York',
+      //     state: 'NY'
+      //   }
+      // },
+      // {
+      //   firstName: 'Peter',
+      //   lastName: 'Hung',
+      //   age: 38,
+      //   address: {
+      //     street: '555 New Road',
+      //     city: 'Princeton',
+      //     state: 'NJ'
+      //   }
+      // }
     ];
 
-  this.addUser({
-    firstName: 'Robert',
-    lastName: 'Chang',
-    age: 24,
-    address: {
-      street: '555 Rainier st',
-      city: 'Dallas',
-      state: 'TX'
-    }
-  })
+    this.showExtended = false;
+
+    // this.addUser({
+    //   firstName: 'Robert',
+    //   lastName: 'Chang',
+    //   age: 24,
+    //   address: {
+    //     street: '555 Rainier st',
+    //     city: 'Dallas',
+    //     state: 'TX'
+    //   }
+    // })
   }
 
   addUser(user: User){
